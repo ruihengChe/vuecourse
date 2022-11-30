35189edd31862129839c3945b7d722e68c598909
Ken <3408238124@qq.com>
2022-11-30 18:25:24 +0800

    配置element-plus

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as elementIcons from '@element-plus/icons-vue';
+for (let iconName in elementIcons) {
+    app.component(iconName, elementIcons[iconName]);
+}
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button>点开弹出框</el-button>
   </div>
 </template>
 
